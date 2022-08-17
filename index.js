0501f0fd9f335f2144f18c6e6e2310d23abdbecf
Andrey Kotlyar <kotlyar562@gmail.com>
2022-08-17 10:56:45 +0300

    implemented a tiny js world

--- index.js
+++ index.js
@@ -5,27 +5,36 @@
    Web app: _put project's github pages URL here_
    */
 
-// ======== OBJECTS DEFINITIONS ========
-// Define your objects here
+function Creature(species, name, gender, legs, hands, saying) {
+  this.species = species;
+  this.name = name;
+  this.gender = gender;
+  this.legs = legs;
+  this.hands = hands;
+  this.saying = saying;
 
+  this.properties = ["species", "name", "gender", "legs", "hands", "saying"];
 
-// ======== OUTPUT ========
-/* Use print(message) for output.
-   Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
+  this.display = function () {
+    if (this.friends) {
+      this.friendsNames = this.friends.map((friend) => friend.name).join(", ");
+      this.properties.push("friendsNames");
+    }
+    print(this.properties.map((prop) => this[prop]).join("; "));
+  };
+}
 
-   Message can contain HTML markup. You may also tweak index.html and/or styles.css.
-   However, please, REFRAIN from improving visuals at least until your code is reviewed
-   so code reviewers might focus on a single file that is index.js.
-   */
+const cat = new Creature("cat", "Jerry", "male", 4, 0, "Myau");
+const dog = new Creature("dog", "Bars", "male", 4, 0, "Woof");
+const woman = new Creature("human", "Maria", "female", 2, 2, "Hi");
+const man = new Creature("human", "Ivan", "male", 2, 2, "Hello");
 
-/* Print examples:
-   print('ABC');
-   print('<strong>ABC</strong>');
-   print('<strong>ABC</strong>', 'div');
+man.friends = [woman, cat, dog];
+woman.friends = [man, cat];
+dog.friends = [man];
 
-   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-   */
+const inhabitants = [man, woman, dog, cat];
 
-
+inhabitants.forEach((creature) => {
+  creature.display();
+});
